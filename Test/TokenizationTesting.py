430930b380f36a44606a790036175064166f469f
AbdulHaseebHussain <a.h.hussain@edu.salford.ac.uk>
2016-10-28 17:01:57 +0100

    Add files via upload

---
+++ Test/TokenizationTesting.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.join('..', 'Src'))
+from Tokenization import TextTokenization
+
+##UNIT TESTS ARE AUTOMATICALLY REGRESSION TESTS
+
+class TextTokenizationTestCase(unittest.TestCase):
+
+	def testSentenceTokenization(self):
+		rawText = "Hello World. How are you?"
+		tokenization = TextTokenization()
+		extractedSentences = tokenization.sentenceTokenize(rawText)
+		expectedSentences = ['Hello World.', 'How are you?']
+		self.assertCountEqual(extractedSentences, expectedSentences)
+
+	def testWordTokenization(self):
+		rawText = "Hello World's. How are' You today in Calledonian-Thistle! 12345"
+		tokenization = TextTokenization()
+		extractedWords  = tokenization.wordTokenize(rawText)
+		expectedWords = ['Hello','World\'s', 'How', 'are\'','You', 'today', 'Calledonian-Thistle']
+		self.assertCountEqual(extractedWords, expectedWords)
+
+if __name__ == '__main__':
+	unittest.main()
